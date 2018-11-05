953c933480ba0ce7dc80fc77a25d2fac23120406
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2018-11-05 11:16:44 -0400

    All Labs

--- L08/ArrayStats.java
+++ L08/ArrayStats.java
@@ -1,38 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package l08;
 
 /**
  *
- * @author s3607751
+ * @author Aitezaz Siddiqi (A00431079)
  */
 public class ArrayStats {
 
-    static int[] createRandomArray(int size, int rand) {
-        
+    private static int[] reqArray;
+
+    static int[] createRandomArray(int size, int max) {
+        reqArray = new int[size];
+        for (int i = 1; i <= size; ++i) {
+            reqArray[i] = 1 + ((int) (Math.random() * max));
+        }
+        return reqArray;
     }
 
     static String getMin(int[] i) {
-
+        return "";
     }
 
     static String getMax(int[] i) {
-
+        return "";
     }
 
     static String getSum(int[] i) {
-
+        return "";
     }
 
     static String getMean(int[] i) {
-
+        return "";
     }
 
     static String getMedian(int[] i) {
-
+        return "";
     }
 
 }
