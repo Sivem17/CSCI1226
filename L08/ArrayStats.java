d4fb0b51dc991628a053161f98f648ff42369dd9
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2018-11-05 12:45:22 -0400

    All Labs

--- L08/ArrayStats.java
+++ L08/ArrayStats.java
@@ -6,34 +6,57 @@ package l08;
  */
 public class ArrayStats {
 
-    private static int[] reqArray;
+    static int[] reqArray;
 
     static int[] createRandomArray(int size, int max) {
         reqArray = new int[size];
-        for (int i = 1; i <= size; ++i) {
-            reqArray[i] = 1 + ((int) (Math.random() * max));
+
+        for (int i = 0; i <= size - 1; ++i) {
+            reqArray[i] = 1 + (int) (Math.random() * max);
         }
         return reqArray;
     }
 
-    static String getMin(int[] i) {
-        return "";
+    static int getMin(int[] i) {
+        int min = i[0];
+        for (int j = 0; j <= i.length - 1; ++j) {
+            if (i[j] <= min) {
+                min = i[j];
+            }
+        }
+        return min;
     }
 
-    static String getMax(int[] i) {
-        return "";
+    static int getMax(int[] i) {
+        int max = i[0];
+        for (int j = 0; j <= i.length - 1; ++j) {
+            if (i[j] >= max) {
+                max = i[j];
+            }
+        }
+        return max;
     }
 
-    static String getSum(int[] i) {
-        return "";
+    static int getSum(int[] i) {
+        int sum = 0;
+        for (int j : i) {
+            sum += j;
+        }
+        return sum;
     }
 
-    static String getMean(int[] i) {
-        return "";
+    static double getMean(int[] i) {
+        int sum = 0;
+        double mean;
+
+        for (int j = 0; j <= i.length - 1; j++) {
+            sum = sum + i[j];
+        }
+        mean = (double) sum / i.length;
+        return mean;
     }
 
-    static String getMedian(int[] i) {
-        return "";
+    static double getMedian(int[] i) {
+        return (double) (i[i.length/2] + i[(i.length-1)/2]) / 2;
     }
-
 }
