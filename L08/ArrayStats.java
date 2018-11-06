beb96bede269f3fc561bcdf7694cbbb2a0bbd204
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2018-11-06 17:16:44 -0400

    All Labs

--- L08/ArrayStats.java
+++ L08/ArrayStats.java
@@ -1,15 +1,23 @@
 package l08;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Aitezaz Siddiqi (A00431079)
  */
 public class ArrayStats {
 
-    static int[] reqArray;
-
-    static int[] createRandomArray(int size, int max) {
-        reqArray = new int[size];
+    /**
+     * Creates an array of required size and fills it with random numbers as max
+     * as its highest value.
+     *
+     * @param size number of element in the array.
+     * @param max highest limit to randomize numbers upto.
+     * @return the array
+     */
+    public static int[] createRandomArray(int size, int max) {
+        int[] reqArray = new int[size];
 
         for (int i = 0; i <= size - 1; ++i) {
             reqArray[i] = 1 + (int) (Math.random() * max);
@@ -17,46 +25,92 @@ public class ArrayStats {
         return reqArray;
     }
 
-    static int getMin(int[] i) {
+    /**
+     * Calculates the minimum value of the array using advanced for loop and
+     * returns it.
+     *
+     * @param i an integer array.
+     * @return minimum value of an array.
+     */
+    public static int getMin(int[] i) {
         int min = i[0];
-        for (int j = 0; j <= i.length - 1; ++j) {
-            if (i[j] <= min) {
-                min = i[j];
+
+        //USING FOREACH LOOP          SAME CAN BE DONE BY NORMAL FOR LOOP
+        for (int j : i) {     //----->(int j = 0; j <= i.length - 1; ++j) {
+            if (j <= min) {   //----->(i[j] <= min) {
+                min = j;      //----->min = i[j];
             }
         }
         return min;
     }
 
-    static int getMax(int[] i) {
+    /**
+     * Calculates the maximum value of the array using advanced for loop and
+     * returns it.
+     *
+     * @param i an integer array.
+     * @return maximum value of an array.
+     */
+    public static int getMax(int[] i) {
         int max = i[0];
-        for (int j = 0; j <= i.length - 1; ++j) {
-            if (i[j] >= max) {
-                max = i[j];
+
+        //USING FOREACH LOOP         SAME CAN BE DONE BY NORMAL FOR LOOP
+        for (int j : i) {    //----->(int j = 0; j <= i.length - 1; ++j) {
+            if (j >= max) {  //----->(i[j] >= max) {
+                max = j;     //----->max = i[j];
             }
         }
         return max;
     }
 
-    static int getSum(int[] i) {
+    /**
+     * Sums up all the element inside the array using advanced for loop and
+     * returns it.
+     *
+     * @param i an integer array.
+     * @return sum of the elements of the array.
+     */
+    public static int getSum(int[] i) {
         int sum = 0;
-        for (int j : i) {
-            sum += j;
+        //USING FOREACH LOOP         SAME CAN BE DONE BY NORMAL FOR LOOP
+        for (int j : i) {    //----->(int j = 0; j <= i.length - 1; j++) {
+            sum += j;        //----->sum += i[j];
         }
         return sum;
     }
 
-    static double getMean(int[] i) {
-        int sum = 0;
-        double mean;
+    /**
+     * Calculates the mean value of the array using advanced for loop and
+     * returns it.
+     *
+     * @param i an integer array.
+     * @return mean value of the array.
+     */
+    public static double getMean(int[] i) {
+        double sum = 0, mean;
 
-        for (int j = 0; j <= i.length - 1; j++) {
-            sum = sum + i[j];
+        //USING FOREACH LOOP         SAME CAN BE DONE BY NORMAL FOR LOOP
+        for (int j : i) {    //----->(int j = 0; j <= i.length - 1; j++) {
+            sum += j;        //----->sum +=i[j];
         }
-        mean = (double) sum / i.length;
+        mean = sum / i.length;
         return mean;
     }
 
-    static double getMedian(int[] i) {
-        return (double) (i[i.length/2] + i[(i.length-1)/2]) / 2;
+    /**
+     * Sorts the array list of the copied array and returns the median.
+     *
+     * @param i an integer array.
+     * @return median of the array.
+     */
+    public static double getMedian(int[] i) {
+
+        int[] j = Arrays.copyOf(i, i.length);
+        Arrays.sort(j);
+        if (j.length % 2 == 0) {
+            return (j[j.length / 2] + j[(j.length / 2) - 1]) / (double) 2;
+        } else {
+            return j[j.length / 2];
+        }
     }
 }
