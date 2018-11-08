d8779a055d89aac5d51690c4b0a9300a6b229081
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2018-11-07 23:54:33 -0400

    All Assignments

--- A08/ArrayOperations.java
+++ A08/ArrayOperations.java
@@ -1,16 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a08;
 
 /**
+ * A class of helper methods for working with arrays.
  *
- * @author s3607751
+ * @author Aitezaz Siddiqi (A00431079)
  */
 public class ArrayOperations {
 
+    /**
+     * Sums up all the element inside the array using advanced for loop and
+     * returns it.
+     *
+     * @param a an integer array.
+     * @return sum of the elements of the array.
+     */
     public static int getSum(int[] a) {
         int sum = 0;
         //USING FOREACH LOOP           SAME CAN BE DONE BY NORMAL FOR LOOP
@@ -20,14 +23,101 @@ public class ArrayOperations {
         return sum;
     }
 
+    /**
+     * Creates another array of size equal to the shorter integer array and
+     * fills its elements by adding the pairwise elements of the two integer
+     * arrays.
+     *
+     * @param a an integer array.
+     * @param b an integer array.
+     * @return the new created array.
+     */
     public static int[] add(int[] a, int[] b) {
-        int[] sumArray = new int[0];
-
-        for (int i = 0; i < a.length && i < b.length; i++) {
+        int size;
+        if (a.length < b.length) {
+            size = a.length;
+        } else {
+            size = b.length;
+        }
+        int[] sumArray = new int[size];
+        for (int i = 0; i < sumArray.length; i++) {
             sumArray[i] = a[i] + b[i];
         }
-
         return sumArray;
+    }
+
+    /**
+     *
+     * Creates another array of size equal to the shorter integer array and
+     * fills its elements by multiplying the pairwise elements of the two
+     * integer arrays.
+     *
+     * @param a an integer array.
+     * @param b an integer array
+     * @return the new created array.
+     */
+    public static int[] multiply(int[] a, int[] b) {
+        int size;
+        if (a.length < b.length) {
+            size = a.length;
+        } else {
+            size = b.length;
+        }
+        int[] reqArray = new int[size];
+        for (int i = 0; i < reqArray.length; i++) {
+            reqArray[i] = a[i] * b[i];
+        }
+        return reqArray;
+    }
+
+    /**
+     * Checks if all the elements in the array are 0 or not.
+     *
+     * @param a an integer array.
+     * @return true if all the elements of the array are 0.
+     */
+    public static boolean allZeroes(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the elements of the shorter array are equal to the first
+     * elements of the longer array.
+     *
+     * @param a an integer array.
+     * @param b an integer array.
+     * @return true if the shorter array has its elements equal to the front
+     * part of the longer array.
+     */
+    public static boolean prefix(int[] a, int[] b) {
+        if (a.length <= b.length) {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[i]) {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Calls the multiply() method to multiply two integer arrays and then calls
+     * the getSum() method to get the sum of the elements of the new array
+     * created by the multiply() method.
+     *
+     * @param a an integer array.
+     * @param b an integer array.
+     * @return dot product of the two integer arrays.
+     */
+    public static int dotProduct(int[] a, int[] b) {
+        return getSum(multiply(a, b));
     }
 
 }
